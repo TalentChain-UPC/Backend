feb168c211cdaab89d3ecda30918ed9896327163
luis_aquije <132310126+LuisAquije123@users.noreply.github.com>
2025-06-04 23:54:42 -0500

    Add company class

---
+++ company-service/src/main/java/com/company/company_service/companies/domain/model/entities/Companies.java
@@ -0,0 +1,29 @@
+package com.company.company_service.companies.domain.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "companies")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+public class Companies {
+
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
