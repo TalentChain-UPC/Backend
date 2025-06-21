891e0be0fa11f8f6a38abd7237fa8a132ba617db
gerardoQuilla <g.quillaluyo@gmail.com>
2025-06-21 02:57:09 -0500

    feat: add web client

--- labor-record-service/src/main/java/com/labor_record/labor_record_service/evidences/application/internal/commandServices/EvidencesCommandServiceImpl.java
+++ labor-record-service/src/main/java/com/labor_record/labor_record_service/evidences/application/internal/commandServices/EvidencesCommandServiceImpl.java
@@ -1,5 +1,6 @@
 package com.labor_record.labor_record_service.evidences.application.internal.commandServices;
 
+import com.labor_record.labor_record_service.evidences.application.internal.outboundServices.acl.ExternalProfileService;
 import com.labor_record.labor_record_service.evidences.domain.model.aggregates.Evidence;
 import com.labor_record.labor_record_service.evidences.domain.model.commands.CreateEvidenceCommand;
 import com.labor_record.labor_record_service.evidences.domain.model.entities.Certificate;
@@ -15,12 +16,15 @@ import java.util.Optional;
 public class EvidencesCommandServiceImpl implements EvidenceCommandService {
     private final EvidencesRepository evidencesRepository;
     private final CertificatesRepository certificatesRepository;
+    private final ExternalProfileService externalProfileService;
 
     public EvidencesCommandServiceImpl(
             EvidencesRepository evidencesRepository,
-            CertificatesRepository certificatesRepository) {
+            CertificatesRepository certificatesRepository,
+            ExternalProfileService externalProfileService) {
         this.evidencesRepository = evidencesRepository;
         this.certificatesRepository = certificatesRepository;
+        this.externalProfileService = externalProfileService;
     }
 
     @Override
@@ -37,6 +41,8 @@ public class EvidencesCommandServiceImpl implements EvidenceCommandService {
         }else{
             if(command.createCertificateCommand().url()=="")return Optional.empty();
             // llamar a microservicio identity y ver si commnad.employeeId existe en la DB
+            var existsEmployee = externalProfileService.verifyIfEmployeeExists(command.createCertificateCommand().employeeId());
+            if(!existsEmployee)return Optional.empty();
             var certificate = new Certificate(command.createCertificateCommand());
             certificatesRepository.save(certificate);
             evidence = new Evidence(command, type, certificate);
